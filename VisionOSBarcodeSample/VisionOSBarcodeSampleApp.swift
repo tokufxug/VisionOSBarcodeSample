0c1fa440ee93544931e9d6a0b583839e4d17fe8c
tokufxug <tokufxug@gmail.com>
2024-03-26 21:47:44 +0900

    Initial Commit

---
+++ VisionOSBarcodeSample/VisionOSBarcodeSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSBarcodeSampleApp.swift
+//  VisionOSBarcodeSample
+//
+//  Created by Sadao Tokuyama on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSBarcodeSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
